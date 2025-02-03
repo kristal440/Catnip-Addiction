7d6fe683c3bdd4b7c03c82ffbfb16d02626a1427
uwuclxdy <filip.zavrnik@gmail.com>
2025-02-03 23:34:46 +0100

    muli -> main

--- Assets/Scripts/UI - Universal/SwitchScene.cs
+++ Assets/Scripts/UI - Universal/SwitchScene.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Photon.Pun;
 
 public class SwitchScene : MonoBehaviour
 {
+    public static void LoadMainMenu()
+    {
+        PhotonNetwork.Disconnect();
+        SceneManager.LoadScene("MainMenu");
+    }
+
     public static void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
