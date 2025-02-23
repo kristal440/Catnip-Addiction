eba2196e332f70e2115e1025a67c365e404f57d4
uwuclxdy <filip.zavrnik@gmail.com>
2025-02-23 23:06:20 +0100

    add checks so it doesn't throw errors

--- Assets/Scripts/Leaderboard/HomeButton.cs
+++ Assets/Scripts/Leaderboard/HomeButton.cs
@@ -7,10 +7,12 @@ public class HomeButton : MonoBehaviour
     public void OnDisconnectButtonClicked()
     {
         // First disconnect from Photon
+        if (PhotonNetwork.InRoom)
+            PhotonNetwork.LeaveRoom();
+        if (PhotonNetwork.InLobby)
+            PhotonNetwork.LeaveLobby();
         if (PhotonNetwork.IsConnected)
-        {
             PhotonNetwork.Disconnect();
-        }
         // Load the main menu scene
         SceneManager.LoadScene("MainMenu");
     }
