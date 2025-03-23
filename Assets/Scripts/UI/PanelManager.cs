2e1da10ea7a86f4aa2208a31bc55b726a5efc219
uwuclxdy <filip.zavrnik@gmail.com>
2025-03-23 21:59:05 +0100

    add support for prefab panels

--- Assets/Scripts/UI/PanelManager.cs
+++ Assets/Scripts/UI/PanelManager.cs
@@ -3,15 +3,43 @@ using UnityEngine;
 public class PanelManager : MonoBehaviour
 {
     public GameObject panel;
+    private GameObject _instantiatedPanel;
 
     public void ClosePanel()
     {
-        if (panel != null)
+        if (_instantiatedPanel)
+        {
+            Destroy(_instantiatedPanel);
+            _instantiatedPanel = null;
+        }
+        else if (panel)
+        {
             panel.SetActive(false);
+        }
     }
+
     public void OpenPanel()
     {
-        if (panel != null)
+        if (!panel)
+            return;
+
+        if (_instantiatedPanel)
+        {
+            _instantiatedPanel.SetActive(true);
+            return;
+        }
+
+        if (!panel.scene.IsValid())
+        {
+            var parent = transform.parent;
+
+            _instantiatedPanel = Instantiate(panel, parent);
+
+            _instantiatedPanel.transform.SetSiblingIndex(transform.GetSiblingIndex() + 1);
+        }
+        else
+        {
             panel.SetActive(true);
+        }
     }
 }
