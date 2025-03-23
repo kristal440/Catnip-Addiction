e02010641507433c97f9118769681abfdfb52cf0
uwuclxdy <filip.zavrnik@gmail.com>
2025-03-23 14:42:01 +0100

    fix death detection after respawning at checkpoint

--- Assets/Scripts/PlayerPrefab/CheckpointManager.cs
+++ Assets/Scripts/PlayerPrefab/CheckpointManager.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
+using static UnityEngine.Vector2;
 
 public class CheckpointManager : MonoBehaviour
 {
     [SerializeField] private string checkpointTag = "Checkpoint"; // tag checkpoint objects, add 2d collider
-    public static Vector3 LastCheckpointPosition { get; private set; }
+    public static Vector2 LastCheckpointPosition { get; private set; }
 
     private void Start()
     {
@@ -13,7 +14,11 @@ public class CheckpointManager : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag(checkpointTag)) return;
-        LastCheckpointPosition = other.transform.position;
+
+        var newCheckpointPosition = other.transform.position;
+
+        if (!(Distance(newCheckpointPosition, LastCheckpointPosition) > 0.01f)) return;
+        LastCheckpointPosition = newCheckpointPosition;
         Debug.Log($"Checkpoint set at {LastCheckpointPosition}");
     }
 }
