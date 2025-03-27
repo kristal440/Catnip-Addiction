4e18e080f0da43a17bea706d2b05471f5a211e8d
uwuclxdy <filip.zavrnik@gmail.com>
2025-03-27 17:53:00 +0100

    optimize script

--- Assets/Scripts/Background/MenuCatRunner.cs
+++ Assets/Scripts/Background/MenuCatRunner.cs
@@ -3,14 +3,11 @@ using UnityEngine;
 public class MenuCatRunner : MonoBehaviour
 {
     private static readonly int Speed = Animator.StringToHash("Speed");
-    private static readonly int JumpTrigger = Animator.StringToHash("Jump");
     private static readonly int SkinParam = Animator.StringToHash("Skin");
 
     private float _speed;
     private float _destroyXPosition;
     private Animator _animator;
-    private bool _hasJumped;
-    private float _nextJumpTime;
     private float _minAnimSpeed;
     private float _maxAnimSpeed;
 
@@ -18,19 +15,15 @@ public class MenuCatRunner : MonoBehaviour
     {
         _speed = speed;
         _destroyXPosition = destroyXPosition;
-        _hasJumped = false;
-        _nextJumpTime = Random.Range(1f, 3f);
         _minAnimSpeed = minAnimSpeed;
         _maxAnimSpeed = maxAnimSpeed;
 
-        if (!gameObject.activeInHierarchy)
-            return;
+        if (!gameObject.activeInHierarchy) return;
 
         if (!_animator)
             _animator = GetComponent<Animator>();
 
-        if (!_animator)
-            return;
+        if (!_animator) return;
 
         _animator.SetInteger(SkinParam, skinVariant);
 
@@ -45,16 +38,8 @@ public class MenuCatRunner : MonoBehaviour
     {
         transform.Translate(Vector3.right * (_speed * Time.deltaTime));
 
-        if (_animator && !_hasJumped && Time.time > _nextJumpTime && Random.value < 0.3f)
-        {
-            _animator.SetTrigger(JumpTrigger);
-            _hasJumped = true;
-        }
-
-        if (transform.position.x > _destroyXPosition)
-        {
-            gameObject.SetActive(false);
-        }
+        if (!(transform.position.x > _destroyXPosition)) return;
+        gameObject.SetActive(false);
     }
 
     public void OnStandingAnimationComplete() { }
